67c101e5eb6391512b60ed267d61fb68f72be8d1
Pravinlkr <pravinlakhisarai@gmail.com>
2021-10-21 22:37:05 +0530

    initialized project

---
+++ src/components/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+   <ion-item>
+    <ion-label>
+      Rahul Kumar
+    </ion-label>
+  </ion-item>
+  <ion-item>
+    <ion-label>
+      Rajat Sharma
+    </ion-label>
+  </ion-item>
+  <ion-item>
+    <ion-label>
+      Raushan Singh
+    </ion-label>
+  </ion-item>
+  <ion-item>
+    <ion-label>
+      Manish Kumar Singh
+    </ion-label>
+  </ion-item>
+</template>
+
+<script>
+export default {
+    name:'UserList'
+}
+</script>
--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,37 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>List Of Users</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">List Of Users</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title>Users</ion-card-title>
+        </ion-card-header>
+        <ion-card-content>
+        <UserList />
+        </ion-card-content>
+      </ion-card>
+      
+      <!-- <ExploreContainer name="List Of Users" /> -->
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent } from '@ionic/vue';
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+import UserList from '@/components/UserList.vue'
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { UserList, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCard, IonCardTitle, IonCardContent }
 }
 </script>
